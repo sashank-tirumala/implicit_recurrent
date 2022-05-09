0f19834e13ad82340561bf1367fa0c71bfa5c506
sashank-tirumala <stsashank6@gmail.com>
2022-05-09 16:05:28 -0400

    Can plot directly from train dataset

--- plot_utils.py
+++ plot_utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 def plot_idx(idx, root, savefig=None):
     rgb_img = np.load(root+"/rgb/"+str(img_num)+".npy")/255.0
-    depth_img = np.load(root+"/depth/"+str(img_num)+".npy")/255.0
+    depth_img = np.load(root+"/depth/"+str(img_num)+".npy")
     imgs = {}
     imgs["orig"] = rgb_img[... ,::-1] #Converts BGR to RGB
     imgs["depth"] = depth_img
@@ -27,7 +27,34 @@ def plot_idx(idx, root, savefig=None):
         plt.savefig(savefig, bbox_inches='tight')
     return None
 
+def plot_sample(sample, savefig=None):
+    rgb_img = sample['rgb'].permute(1,2,0).numpy()
+    depth_img = sample['X'][0, :, :].numpy()
+    imgs = {}
+    imgs["orig"] = rgb_img[..., ::-1] #Converts BGR to RGB
+    print(imgs["orig"].shape)
+    imgs["depth"] = depth_img
+    for i in range(sample['Y'].shape[0]):
+        imgs[str(i)] = sample['Y'][i,:,:].numpy()
+    rows = 1
+    cols = len(imgs.keys())
+    fig = plt.figure()
+    ax = []
+    for i in range(cols*rows):
+        name = list(imgs.keys())[i]
+        ax.append( fig.add_subplot(rows, cols, i+1) )
+        ax[-1].set_title(name)  # set title
+        plt.imshow(imgs[name])
+    if(savefig == None):
+        plt.show()
+    else:
+        plt.savefig(savefig, bbox_inches='tight')
+    return None
+
 if(__name__ == "__main__"):
-    path = "/media/YertleDrive4/layer_grasp/dataset/2cloth_rec/train"
+    path = "/media/YertleDrive4/layer_grasp/dataset/2cloth_rec/test"
     img_num = 125
-    plot_idx(img_num, path)
+    from data_loader import RecClothDataset
+    train_data = RecClothDataset(root_dir="/media/YertleDrive4/layer_grasp/dataset/2cloth_rec/train", num_masks=2, use_transform=True)
+    sample = train_data[img_num]
+    plot_sample(sample)
