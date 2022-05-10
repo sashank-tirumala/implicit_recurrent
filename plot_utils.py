9068521d5d38a4c972e5df94ca29409c81964391
sashank-tirumala <stsashank6@gmail.com>
2022-05-09 22:20:16 -0400

    Fixed indentation errors and closed figure after saving

--- plot_utils.py
+++ plot_utils.py
@@ -2,54 +2,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 def plot_idx(idx, root, savefig=None):
-    rgb_img = np.load(root+"/rgb/"+str(img_num)+".npy")/255.0
-    depth_img = np.load(root+"/depth/"+str(img_num)+".npy")
-    imgs = {}
-    imgs["orig"] = rgb_img[... ,::-1] #Converts BGR to RGB
-    imgs["depth"] = depth_img
-    mask_dir = root+"/masks"
-    maskdirs = os.listdir(mask_dir)
-    for i in range(len(maskdirs)):
-        img =  np.load(root+"/masks/"+maskdirs[i]+"/"+str(img_num)+".npy")
-        imgs[str(maskdirs[i])] = img
-    rows = 1
-    cols = len(imgs.keys())
-    fig = plt.figure()
-    ax = []
-    for i in range(cols*rows):
-        name = list(imgs.keys())[i]
-        ax.append( fig.add_subplot(rows, cols, i+1) )
-        ax[-1].set_title(name)  # set title
-        plt.imshow(imgs[name])
-    if(savefig == None):
-        plt.show()
-    else:
-        plt.savefig(savefig, bbox_inches='tight')
-    return None
+	rgb_img = np.load(root+"/rgb/"+str(img_num)+".npy")/255.0
+	depth_img = np.load(root+"/depth/"+str(img_num)+".npy")
+	imgs = {}
+	imgs["orig"] = rgb_img[... ,::-1] #Converts BGR to RGB
+	imgs["depth"] = depth_img
+	mask_dir = root+"/masks"
+	maskdirs = os.listdir(mask_dir)
+	for i in range(len(maskdirs)):
+		img =  np.load(root+"/masks/"+maskdirs[i]+"/"+str(img_num)+".npy")
+		imgs[str(maskdirs[i])] = img
+	rows = 1
+	cols = len(imgs.keys())
+	fig = plt.figure()
+	ax = []
+	for i in range(cols*rows):
+		name = list(imgs.keys())[i]
+		ax.append( fig.add_subplot(rows, cols, i+1) )
+		ax[-1].set_title(name)  # set title
+		plt.imshow(imgs[name])
+	if(savefig == None):
+		plt.show()
+	else:
+		plt.savefig(savefig, bbox_inches='tight')
+	return None
 
 def plot_sample(sample, savefig=None):
-    rgb_img = sample['rgb'].permute(1,2,0).numpy()
-    depth_img = sample['X'][0, :, :].numpy()
-    imgs = {}
-    imgs["orig"] = rgb_img[..., ::-1] #Converts BGR to RGB
-    print(imgs["orig"].shape)
-    imgs["depth"] = depth_img
-    for i in range(sample['Y'].shape[0]):
-        imgs[str(i)] = sample['Y'][i,:,:].numpy()
-    rows = 1
-    cols = len(imgs.keys())
-    fig = plt.figure()
-    ax = []
-    for i in range(cols*rows):
-        name = list(imgs.keys())[i]
-        ax.append( fig.add_subplot(rows, cols, i+1) )
-        ax[-1].set_title(name)  # set title
-        plt.imshow(imgs[name])
-    if(savefig == None):
-        plt.show()
-    else:
-        plt.savefig(savefig, bbox_inches='tight')
-    return None
+	rgb_img = sample['rgb'].permute(1,2,0).numpy()
+	depth_img = sample['X'][0, :, :].numpy()
+	imgs = {}
+	imgs["orig"] = rgb_img[..., ::-1] #Converts BGR to RGB
+	print(imgs["orig"].shape)
+	imgs["depth"] = depth_img
+	for i in range(sample['Y'].shape[0]):
+		imgs[str(i)] = sample['Y'][i,:,:].numpy()
+	rows = 1
+	cols = len(imgs.keys())
+	fig = plt.figure()
+	ax = []
+	for i in range(cols*rows):
+		name = list(imgs.keys())[i]
+		ax.append( fig.add_subplot(rows, cols, i+1) )
+		ax[-1].set_title(name)  # set title
+		plt.imshow(imgs[name])
+	if(savefig == None):
+		plt.show()
+	else:
+		plt.savefig(savefig, bbox_inches='tight')
+	return None
 
 def make_plot(outp, y, rgb, x, savefig=None):
 	cols = 5
@@ -83,15 +83,17 @@ def make_plot(outp, y, rgb, x, savefig=None):
 			plt.imshow(y[0,int(i/5),:,:])
 	if(savefig == None):
 		plt.savefig("inp_outp_plot")
+		plt.close()
 	else:
 		plt.savefig(savefig)
+		plt.close()
 
 def plot_network():
-    pass
+	pass
 if(__name__ == "__main__"):
-    path = "/media/YertleDrive4/layer_grasp/dataset/2cloth_rec/test"
-    img_num = 166
-    from data_loader import RecClothDataset
-    train_data = RecClothDataset(root_dir="/media/YertleDrive4/layer_grasp/dataset/2cloth_rec/train", num_masks=2, use_transform=True)
-    sample = train_data[img_num]
-    plot_sample(sample)
+	path = "/media/YertleDrive4/layer_grasp/dataset/2cloth_rec/test"
+	img_num = 166
+	from data_loader import RecClothDataset
+	train_data = RecClothDataset(root_dir="/media/YertleDrive4/layer_grasp/dataset/2cloth_rec/train", num_masks=2, use_transform=True)
+	sample = train_data[img_num]
+	plot_sample(sample)
